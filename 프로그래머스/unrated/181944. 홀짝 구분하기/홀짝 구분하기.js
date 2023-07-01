df20d0fe25ded2b059dcfba0f2dfdc6e1812bffe
suny0ung <121158293+suny0ung@users.noreply.github.com>
2023-07-01 17:06:50 +0900

    Add: 좋은 코드 예시 주석으로 추가 (Update 홀짝 구분하기.js)

--- 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.js
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.js
@@ -16,3 +16,13 @@ rl.on('line', function (line) {
         console.log(`${num} is odd`);
     }
 });
+
+//다른사람의 좋은 코드 예시
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// }).on('line', function (line) {
+//     const result = Number(line) % 2 ? 'odd' : 'even'
+//     console.log(line, 'is', result)
+// })

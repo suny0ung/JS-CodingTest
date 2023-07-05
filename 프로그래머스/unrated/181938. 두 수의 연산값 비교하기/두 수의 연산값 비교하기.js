df612bf0b619810551ea3fa3adeeef5baecc3e64
suny0ung <121158293+suny0ung@users.noreply.github.com>
2023-07-05 10:59:20 +0900

    Update 두 수의 연산값 비교하기.js

--- 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
+++ 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
@@ -4,3 +4,16 @@ function solution(a, b) {
     
     return Math.max(Number(addNum), multiNum)
 }
+
+// 다른 사람의 코드예시
+// function solution(a, b) {
+//     return Math.max(Number(`${a}${b}`), a * b * 2)
+// }
+
+
+// 다른 사람의 코드 예시
+// function solution(a, b) {
+//     var answer = parseInt(`${a}${b}`)
+//     var answer1 =2*a*b;
+//     return answer > answer1 ? answer : answer1;
+// }

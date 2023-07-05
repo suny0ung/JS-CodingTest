596ecd81d52b266893f847cc65a88c7f02e19b88
suny0ung <121158293+suny0ung@users.noreply.github.com>
2023-07-05 11:08:57 +0900

    Update 공배수.js

--- 프로그래머스/unrated/181936. 공배수/공배수.js
+++ 프로그래머스/unrated/181936. 공배수/공배수.js
@@ -5,3 +5,14 @@ function solution(number, n, m) {
         return 0;
     }
 }
+
+
+//다른사람의 풀이
+// function solution(number, n, m) {
+//     return (number%n ===0) ? (number%m===0) ? 1 : 0 : 0;
+// }
+
+
+// function solution(number, n, m) {
+//   return +!(number % n || number % m);
+// }

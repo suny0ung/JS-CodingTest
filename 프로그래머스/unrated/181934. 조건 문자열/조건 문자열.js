8c23783b612f3d6299d6cf6d8eece523dfe29bb4
suny0ung <121158293+suny0ung@users.noreply.github.com>
2023-07-10 22:16:27 +0900

    Update 조건 문자열.js

--- 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -14,3 +14,25 @@ function solution(ineq, eq, n, m) {
     return 0;
 
 }
+
+//다른사람의 좋은 조건 예제
+//첫번째
+function solution(ineq, eq, n, m) {
+    if (eq === '=' && n === m) return 1
+    if (ineq === '<' && n < m) return 1
+    if (ineq === '>' && n > m) return 1
+    return 0
+}
+
+//두번째
+const operations = {
+  '>=': (n, m) => n >= m,
+  '<=': (n, m) => n <= m,
+  '>!': (n, m) => n > m,
+  '<!': (n, m) => n < m,
+};
+
+function solution(ineq, eq, n, m) {
+  const op = operations[ineq + eq];
+  return Number(op(n, m));
+}

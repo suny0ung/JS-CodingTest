9ba524825a081d0bf69c51462f67f0762e47a945
suny0ung <121158293+suny0ung@users.noreply.github.com>
2023-07-11 12:08:39 +0900

    Update 나머지가 1이 되는 수 찾기.js

--- 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -9,3 +9,16 @@ function solution(n) {
         }
     }
 }
+
+//다른사람의 좋은 문제풀이 예시
+function solution(n, x = 1) {    
+    while (x++) {
+        if (n % x === 1) {
+            return x;
+        }
+    }    
+}
+
+// function solution(n, x = 0) {
+//     return n % x === 1 ? x : solution(n, x + 1);
+// }
